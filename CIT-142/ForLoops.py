7aef658f251194ae6a318dd20aeed3b9df965ced
Evan Kirsch <40045743+EvanKirsch@users.noreply.github.com>
2025-10-27 16:08:44 -0500

    Update ForLoops.py

--- CIT-142/ForLoops.py
+++ CIT-142/ForLoops.py
@@ -1,1 +1,11 @@
-
+def my_list_formatter(my_list, index_to_highlight=-1):
+    border = " "
+    center = "| "
+    for i in range(len(my_list)):
+        char_length = len(str(my_list[i]))
+        border_char = "-"
+        if (i == index_to_highlight):
+            border_char = "*"
+        border = border + border_char * (char_length + 2)  + " "
+        center = center + str(my_list[i]) + " | "
+    return border + '\n' + center + '\n' + border
